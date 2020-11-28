bc7f0c4ec79f804d3c08b2cc24f5c6edaf127305
Fabio Almeida <fabioalmeida@Fabios-MBP.home>
2020-11-28 12:50:59 -0500

    registration form basic layout and functionality

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import store from "./store/index";
+import { VueMaskDirective } from 'v-mask'
 
 Vue.config.productionTip = false;
+
+Vue.directive('mask', VueMaskDirective);
+
 
 new Vue({
   router,
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -9,58 +9,98 @@
       <v-card-title>
         Let's get you registered.
       </v-card-title>
-      <v-card-text>
-        <v-text-field label="Name" />
-        <v-text-field label="DOB" />
-        <v-text-field label="Graduating year" />
-        <v-text-field label="Email" />
-        <v-text-field label="Parent or guardian's e-mail" />
-        <v-checkbox label="Accepted Verbal Offer" />
-        <v-checkbox label="Accepted Written Offer" />
-        <v-text-field v-if="!autocomplete" label="Enter Your Gym Name" />
-        <v-autocomplete 
-          v-if="autocomplete" 
-          label="Gym" 
-          v-model="gym" 
-          full-width  
-          :items="gyms" 
-          item-text="name" 
-          item-value="name" 
-          no-data-text=""
-          :search-input.sync="search"
-        >
-          <!-- <template v-slot:no-data>
-            <div>Your gym is not in the list?</div>
-            <v-btn class="mx-5" text @click="addNewGym()">Click here to add it now</v-btn>
-          </template> -->
+  
+      <v-form lazy-validation v-model="valid">
+        <v-card-text>
+          <h4>Basic Info</h4>
+            <v-text-field label="Full name" v-model="fullName" :rules="[...validation.required]" required />
+            <v-row>
+              <v-col md="8" sm="12" class="v-flex">
+                <v-text-field label="DOB"  v-mask="'##/##/####'" v-model="dob" :rules="[...validation.required]" required/>
+              </v-col>
+              <v-col md="4" sm="12">
+                <v-select :items="graduatingYears" label="Grad year" required></v-select>
+              </v-col>
+            </v-row>
+            <v-text-field label="Email" :rules="[...validation.required, ...validation.email]" />
+          
+          <h4>Parent or Guardian</h4>
+            <v-text-field label="Legal guardian's name" :rules="[...validation.required]"/>
+            <v-text-field label="Legal guardian's e-mail" :rules="[...validation.required, ...validation.email]"/>
 
-          <template v-slot:item="data">
-            <template v-if="typeof data.item !== 'object'">
+          <h4>Gym and coaches</h4>
+            <v-autocomplete 
+              v-if="autocomplete" 
+              label="Gym" 
+              v-model="gym" 
+              full-width  
+              :items="gyms" 
+              item-text="name" 
+              item-value="name" 
+              no-data-text=""
+              :search-input.sync="search"
+              :rules="[...validation.required]"
+            >
+            <!-- <template v-slot:no-data>
+              <div>Your gym is not in the list?</div>
+              <v-btn class="mx-5" text @click="addNewGym()">Click here to add it now</v-btn>
+            </template> -->
+
+            <template v-slot:item="data">
+              <template v-if="typeof data.item !== 'object'">
+              </template>
+              <template v-else>
+                <v-list-item-avatar>
+                  <img :src="data.item.avatar">
+                </v-list-item-avatar>
+                <v-list-item-content>
+                  <v-list-item-title v-html="data.item.name"></v-list-item-title>
+                  <v-list-item-subtitle v-html="data.item.group"></v-list-item-subtitle>
+                </v-list-item-content>
+              </template>
             </template>
-            <template v-else>
-              <v-list-item-avatar>
-                <img :src="data.item.avatar">
-              </v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title v-html="data.item.name"></v-list-item-title>
-                <v-list-item-subtitle v-html="data.item.group"></v-list-item-subtitle>
-              </v-list-item-content>
-            </template>
-          </template>
-        </v-autocomplete>  
-        <v-text-field label="Coach's e-mail" />
-        <v-text-field label="Youtube channel" />
-        <v-text-field label="Instagram Account" />
+          </v-autocomplete>
 
-        
-      </v-card-text>
-      <v-footer>
-        <v-card-actions></v-card-actions>
-      </v-footer>
-      <v-form>
+            <v-text-field label="Head Coachs full name" :rules="[...validation.required]"/>
+            <v-text-field label="Head Coachs email" :rules="[...validation.required, ...validation.email]"/>
 
+          <h4>Social Media</h4>
+          <v-text-field label="Youtube channel" />
+          <v-text-field label="Instagram Account" />
+          
+          <h4>Intent</h4>
+            <v-checkbox label="Accepted Verbal Offer" />
+            <v-checkbox label="Accepted Written Offer" />
+
+          <h4>Password</h4>
+            <v-text-field 
+              label="Password" 
+              v-model="password" 
+              :rules="[...validation.required, ...validation.passwordLength]"
+              :type="revealPassword ? 'password' : 'text'"
+            />
+            <v-text-field
+             label="Re enter your password"
+              :rules="[...validation.required, ...validation.pwdMatchesRules]"
+              :append-icon="revealPassword ? 'mdi-eye-off' : 'mdi-eye'"
+              @click:append="() => (revealPassword = !revealPassword)"
+              :type="revealPassword ? 'password' : 'text'"
+            />
+        </v-card-text>
+        <v-footer>
+          <v-card-actions >
+            <v-btn color="info" width="100%"> Register user </v-btn>
+          </v-card-actions>
+        </v-footer>
       </v-form>
     </v-card>
+
+    <v-snackbar 
+      v-model="snackbar"
+      color="red"
+    >
+      {{this.snackbarMessage}}
+    </v-snackbar>
   </v-container>
 </template>
 
@@ -69,22 +109,63 @@
   import axios from "axios";
   //@ts-ignore
   import api from "@/api/server-api";
+  import router from '@/router';
 
   export default Vue.extend({
     name: "Register",
 
-    data: () => ({
-      autocomplete: true,
-      search: null, 
-      addGym: [],
-      gym:"",
-      gyms: [
-        {name:'Port Jefferson Gymnastics', type: "from-server"}, 
-        {name:'Apex Athletics'}, 
+    data(){
+      return {
+        revealPassword: true,
+        valid: true ,
+        snackbar: false,
+        snackbarMessage: "",
+        fullName:"",
+        dob:"",
+        email:"",
+        guardianName:"",
+        guardiaEmail:"",
+        gym:"",
+        coachesName:"",
+        coachEmail:"",
+        youtubeChannel:"",
+        instagramAccount:"",
+        acceptedVerbalOffer:"",
+        acceptedWriottenOffer:"",
+        password:"",
+        autocomplete: true,
+        search: null, 
+        addGym: [],
+        gyms: [
+          {name:'Port Jefferson Gymnastics', type: "from-server"}, 
+          {name:'Apex Athletics'}, 
         ],
-    }),
+        validation: {
+          required: [(v: any) => !!v || "Required"],
+          email: [(v: any) => !!v || "E-mail is required", (v: any) => /.+@.+\..+/.test(v) || "E-mail must be valid"],
+          passwordLength: [(v: any) => v.length >= 8 || "Password must be at least 8 characters long"],
+          pwdMatchesRules: [(v: any) => v == this.$data.password || 'Password does not match'],
+        }
+      }
+    },
     methods: {
- 
+      async validate() {
+        // @ts-ignore
+        this.valid = this.$refs.form.validate();
+        const token = this.$route.query.t;
+        if (this.valid) {
+          await this.$store.dispatch('register', token) 
+          .then((res)=>{
+            if (res.data.status == 'success'){
+              router.push("/login")
+            }else{
+              this.snackbar = true;
+              this.snackbarMessage = `There was a problem trying to register you at this time. Please verify your information and try again.`;
+
+            }
+        });  
+        }
+      }, 
     },
     watch: {
       search(val: any){
@@ -100,11 +181,24 @@
       }
     },
     computed: {
-      
+      graduatingYears() {
+        const years = [];
+        const num_of_years = 20;
+        const first_year = 2020;
+        for (let i = first_year; i <= first_year + num_of_years; i++) {
+          years.push(i);
+        }
+        return years;
+      },
     },
   });
 </script>
 
 <style lang="scss">
-  
+  .v-flex{
+    display: flex;
+  }
+  .dob-flex-fields >>> input{
+    text-align: center;
+  }
 </style>
